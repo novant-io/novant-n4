17136bdb400839f2c3b90aa5909a2e8879957997
Andy Frank <afrankvt@gmail.com>
2021-05-13 16:18:18 -0400

    Remove httpConfig
    I don't think this is something we want/need

--- novant/novant-rt/src/io/novant/BNovantNetwork.java
+++ novant/novant-rt/src/io/novant/BNovantNetwork.java
@@ -8,7 +8,6 @@
 
 package io.novant;
 
-//import javax.baja.license.Feature;
 import javax.baja.naming.BOrd;
 import javax.baja.sys.*;
 import javax.baja.util.Lexicon;
@@ -21,84 +20,52 @@ import com.tridium.ndriver.datatypes.*;
 import com.tridium.ndriver.discover.*;
 import com.tridium.ndriver.poll.*;
 
-
-
 /**
- *  BNovantNetwork models a network of devices
- *
- *  @author   Novant LLC
- *  @creation 13-May-21
+ * BNovantNetwork models a network of Novant devices
  */
 @NiagaraType
   @NiagaraProperty(name = "pollScheduler", type = "BNPollScheduler",  defaultValue = "new BNPollScheduler()")
-  @NiagaraProperty(name = "httpConfig", type = "BHttpCommConfig",  defaultValue = "new BHttpCommConfig()")
 
 public class BNovantNetwork
   extends BNNetwork
 {
+
 /*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
+/*@ $io.novant.BNovantNetwork(514687877)1.0$ @*/
+/* Generated Thu May 13 16:14:49 EDT 2021 by Slot-o-Matic (c) Tridium, Inc. 2012 */
 
 ////////////////////////////////////////////////////////////////
-//Property "pollScheduler"
+// Property "pollScheduler"
 ////////////////////////////////////////////////////////////////
 
-    /**
-     * Slot for the <code>pollScheduler</code> property.
-     * @see io.novant.BNovantNetwork#getPollScheduler
-     * @see io.novant.BNovantNetwork#setPollScheduler
-     */
-    public static final Property pollScheduler = newProperty(0, new BNPollScheduler(),null);
+  /**
+   * Slot for the {@code pollScheduler} property.
+   * @see #getPollScheduler
+   * @see #setPollScheduler
+   */
+  public static final Property pollScheduler = newProperty(0, new BNPollScheduler(), null);
 
-    /**
-     * Get the <code>pollScheduler</code> property.
-     * @see io.novant.BNovantNetwork#pollScheduler
-     */
-    public BNPollScheduler getPollScheduler() { return (BNPollScheduler)get(pollScheduler); }
+  /**
+   * Get the {@code pollScheduler} property.
+   * @see #pollScheduler
+   */
+  public BNPollScheduler getPollScheduler() { return (BNPollScheduler)get(pollScheduler); }
 
-    /**
-     * Set the <code>pollScheduler</code> property.
-     * @see io.novant.BNovantNetwork#pollScheduler
-     */
-    public void setPollScheduler(BNPollScheduler v) { set(pollScheduler,v,null); }
+  /**
+   * Set the {@code pollScheduler} property.
+   * @see #pollScheduler
+   */
+  public void setPollScheduler(BNPollScheduler v) { set(pollScheduler, v, null); }
 
+////////////////////////////////////////////////////////////////
+// Type
+////////////////////////////////////////////////////////////////
 
-  ////////////////////////////////////////////////////////////////
-  // Property "httpConfig"
-  ////////////////////////////////////////////////////////////////
-
-    /**
-     * Slot for the <code>httpConfig</code> property.
-     * @see io.novant.BNovantNetwork#getHttpConfig
-     * @see io.novant.BNovantNetwork#setHttpConfig
-     */
-    public static final Property httpConfig = newProperty(0, new BHttpCommConfig(),null);
-
-    /**
-     * Get the <code>httpConfig</code> property.
-     * @see io.novant.BNovantNetwork#httpConfig
-     */
-    public BHttpCommConfig getHttpConfig() { return (BHttpCommConfig)get(httpConfig); }
-
-    /**
-     * Set the <code>httpConfig</code> property.
-     * @see io.novant.BNovantNetwork#httpConfig
-     */
-    public void setHttpConfig(BHttpCommConfig v) { set(httpConfig,v,null); }
-
-
-
-  ////////////////////////////////////////////////////////////////
-  // Type
-  ////////////////////////////////////////////////////////////////
-
+  @Override
   public Type getType() { return TYPE; }
   public static final Type TYPE = Sys.loadType(BNovantNetwork.class);
 
-  /*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
-
-
-
-
+/*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
 
   /** Specify name for network resources. */
   public String getNetworkName() { return "NovantNetwork"; }
@@ -117,45 +84,22 @@ public class BNovantNetwork
     return BNovantDevice.TYPE;
   }
 
-  /** TODO - Add license check if needed
-  @Override
-  public final Feature getLicenseFeature()
-  {
-    return Sys.getLicenseManager().getFeature("?? vendor", "?? feature");
-  }
-  */
-
   @Override
   public void changed(Property p, Context cx)
   {
     super.changed(p, cx);
-    if(!isRunning()) return;
+    if (!isRunning()) return;
 
-    if(p == status)
+    if (p == status)
     {
       // Give any comms opportunity to respond to status changes
-      getHttpConfig().statusUpdate();
-
+      // getHttpConfig().statusUpdate();
     }
   }
 
-
-
-
 ////////////////////////////////////////////////////////////////
-//Utilities
+// Utilities
 ////////////////////////////////////////////////////////////////
-
-  /** Access the http comm stack */
-  public HttpComm hcomm()
-  {
-    return (HttpComm)getHttpConfig().comm();
-  }
-
-
-
-
 
   public static Lexicon LEX = Lexicon.make(BNovantNetwork.class);
-
 }
